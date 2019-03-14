e0b5bd9f79da1f5181f951cf5a4c34f227effe69
Trevor Facer <trevorf@ifit.com>
2019-03-14 13:27:43 -0600

    Remove puma from start script
    We are now using nginx for handling ssl termination, so we no longer
    need to use puma.

--- start-txgh.sh
+++ start-txgh.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-SSL_KEY="/home/ec2-user/.ssh/key.pem"
-SSL_CERT="/home/ec2-user/.ssh/cert.pem"
 PORT=9292
 
-SSL_STARTUP_URI="ssl://0.0.0.0:$PORT?key=$SSL_KEY&cert=$SSL_CERT"
-
-setsid puma -b "$SSL_STARTUP_URI" &>>out.log &
+setsid bundle exec rackup --host 0.0.0.0 -E production -p ${PORT} &>> out.log &
