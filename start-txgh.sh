3b3e89dcb0e2428048de90e3e67aee1e869be12a
Trevor Facer <trevorf@ifit.com>
2019-03-14 13:32:23 -0600

    Update start script to incorporate nginx
    This will check for nginx changes, then copy the new config into
    place if necessary.  It will then attempt to restart nginx.

--- start-txgh.sh
+++ start-txgh.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
 PORT=9292
+LOG_FILE=./out.log
 
+function tee_logs {
+  echo "$@" | tee -a "${LOG_FILE}"
+}
+
+tee_logs "Starting txgh"
 setsid bundle exec rackup --host 0.0.0.0 -E production -p ${PORT} &>> out.log &
+
+tee_logs "Checking for nginx config changes"
+NGINX_CHANGES=$(diff ./nginx/ssl.conf /etc/nginx/conf.d/ssl.conf)
+if [[ -n ${NGINX_CHANGES} ]]; then
+  tee_logs "Nginx changed.  Backing up config and copying new config into place."
+  cp /etc/nginx/conf.d/ssl.conf{,.bak}
+  cp ./nginx/ssl.conf /etc/nginx/conf.d/ssl.conf
+fi
+
+tee_logs "Reloading nginx"
+nginx -s reload
